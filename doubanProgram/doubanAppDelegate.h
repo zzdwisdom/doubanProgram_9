878dd3657673e60b5591a93ee907234d49dc1a6f
MrXir <mrxir@MrXirtekiMacBook-Pro.local>
2013-04-23 19:34:25 +0800

    Initial Commit

---
+++ doubanProgram/doubanAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  doubanAppDelegate.h
+//  doubanProgram
+//
+//  Created by MrXir on 13-4-23.
+//  Copyright (c) 2013年 MrXir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface doubanAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ doubanProgram/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  doubanProgram
+//
+//  Created by MrXir on 13-4-23.
+//  Copyright (c) 2013年 MrXir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "doubanAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([doubanAppDelegate class]));
+    }
+}
