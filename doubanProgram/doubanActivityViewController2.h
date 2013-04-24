9aa85dc0ef40b8203320f9136eb16d5d73d2e36b
徐 杨 <Young@jotekiMacBook-Air.local>
2013-04-24 11:25:53 +0800

    添加第三方类库uiimageKIT 和三个framework

---
+++ doubanProgram/doubanActivityViewController2.h
@@ -0,0 +1,13 @@
+//
+//  doubanActivityViewController2.h
+//  doubanProgram
+//
+//  Created by 徐 杨 on 13-4-24.
+//  Copyright (c) 2013年 MrXir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface doubanActivityViewController2 : UIViewController
+
+@end
---
+++ doubanProgram/doubanActivityViewController2.m
@@ -0,0 +1,38 @@
+//
+//  doubanActivityViewController2.m
+//  doubanProgram
+//
+//  Created by 徐 杨 on 13-4-24.
+//  Copyright (c) 2013年 MrXir. All rights reserved.
+//
+
+#import "doubanActivityViewController2.h"
+
+@interface doubanActivityViewController2 ()
+
+@end
+
+@implementation doubanActivityViewController2
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
